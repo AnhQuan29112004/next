c75cbeb12f563463afc7a63f82924769e6354d60
AnhQuan29112004 <namcaodidong2004@gmail.com>
2024-08-10 12:50:42 +0700

    ,

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,7 +3,15 @@ type Params = {
     slug: string;
   };
 };
+export async function generateStaticParams() {
+  // Giả sử bạn có một danh sách các slug từ API hoặc từ một nguồn dữ liệu
+  const slugs = ["slug1", "slug2", "slug3"];
 
+  // Trả về danh sách các slug dưới dạng các object chứa `params`
+  return slugs.map(slug => ({
+    slug
+  }));
+}
 export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` };
 }
