e7dcebcd13b6569d0281c852047c4bf31b59aee8
AnhQuan29112004 <namcaodidong2004@gmail.com>
2024-08-10 12:47:14 +0700

    .

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,16 +1,13 @@
-// app/blog/[slug]/page.tsx
+type Params = {
+  params: {
+    slug: string;
+  };
+};
 
-export async function generateStaticParams() {
-  // Giả sử bạn có một danh sách các slug từ API hoặc từ một nguồn dữ liệu
-  const slugs = ["slug1", "slug2", "slug3"];
-
-  // Trả về danh sách các slug dưới dạng các object chứa `params`
-  return slugs.map(slug => ({
-    slug
-  }));
+export async function generateMetadata({ params }: Params) {
+  return { title: `Post: ${params.slug}` };
 }
 
-export default function BlogPost({ params }: { params: { slug: string } }) {
-  // Nội dung của trang blog dựa trên `params.slug`
-  return <div>Slug: {params.slug}</div>;
+export default function Page({ params }: Params) {
+  return <h1>Slug: {params.slug}</h1>;
 }
